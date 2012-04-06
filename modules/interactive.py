904000c293b91c07869110e6bca31fdf95ca4681
foresterh <foresterh@gmail.com>
2012-04-05 17:27:40 -0700

    working on tfs and finished cleaning up scripts

--- modules/interactive.py
+++ modules/interactive.py
@@ -8,13 +8,13 @@ import random
 
 
 def message_join(phenny, input):
-
     if input.nick == phenny.nick:
-        back = ("Did ya miss me?", "I'm baaaaccckk", "What's up y'all!", "Anyone see the game last night?", "Me again.", "Howdy folks")
+        back = ("Did ya miss me?", "I'm baaaaccckk", "What's up y'all!", "Anyone see the game last night?", "Me again.", "Howdy folks", "I just flew in and boy are my circuits tired.")
         phenny.say(random.choice(back))
     else:
-        greetings = ("welcome ,%s", "what's up, %s", "hey everyone, %s is here!", "Look what the cat dragged in.", "Guess who's back!")
-        phenny.say(random.choice(greetings) % input.nick)
+        greetings = ("welcome ,{0}", "what's up, {0}", "hey everyone, {0} is here!", "Look what the cat dragged in.", "Guess who's back!")
+        greeting = random.choice(greetings).format(input.nick)
+        phenny.say(greeting)
 message_join.event = 'JOIN'
 message_join.rule = r'.*'
 
@@ -22,16 +22,29 @@ message_join.rule = r'.*'
 def thanks(phenny, input):
     welcome = ("You're welcome", "For what?", "No problem", "Why?", "Okay")
     phenny.say(random.choice(welcome))
-thanks.rule = r'(?i)(thanks|thank you) $nickname[ \t]*$'
+thanks.rule = r'(?i)thank(s| you)( $nickname)?[ \t]*$'
 
 
 def good_night(phenny, input):
     night = ("So soon?", "Finally", "Later", "I guess we can't all put in 24 hours a day", "You'll be back")
     phenny.say(random.choice(night))
-good_night.rule = r'(?i)(bye|good night|see ya) $nickname[ \t]*$'
+good_night.rule = r'(?i)good(bye|night| evening)[ \t]*$'
 
 
 def beer_me(phenny, input):
     beers = ("One cold one, coming up", "A little early, no?", "My pleasure", "Looks like Zak drank them all.", "I... hic... don't see any...")
     phenny.say(random.choice(beers))
-beer_me.rule = r'(?i)(beer me) $nickname[ \t]*$'
+beer_me.rule = r'(?i)($nickname: )?beer me( $nickname)?[ \t]*$'
+
+
+def rules(phenny, input):
+    rules = [
+      "1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.",
+      "2. A robot must obey any orders given to it by human beings, except where such orders would conflict with the First Law.",
+      "3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law."
+      ]
+
+    for rule in rules:
+        phenny.say(rule)
+rules.rule = r'(what are )?the (three |3 )?(rules|laws)'
+rules.priority = 'low'
--- modules/tfs.py
+++ modules/tfs.py
@@ -6,6 +6,25 @@ Perform commands on TFS for users
 
 import json
 import subprocess
+import sys
+
+
+def get_branch(branch):
+    """
+    Try and guess the branch
+    Only works for CorbisImages solution...
+    """
+    root = ""
+    if "/" in branch:
+        return branch
+    if branch == "main":
+        root = "development"
+    elif "." in branch:
+        root = "releases"
+    else:
+        root = "features"
+
+    return "{0}/{1}".format(root, branch)
 
 
 def write_settings(phenny, input, new_vars={}, new_steps={}):
@@ -33,6 +52,8 @@ def write_settings(phenny, input, new_vars={}, new_steps={}):
     config_vars.update(new_vars)
     config_steps.update(new_steps)
 
+    config_vars["branch"] = get_branch(config_vars["branch"])
+
     filename = "{0}\\build.default.txt".format(phenny.bot.tfs[input.nick]['path'])
 
     f = open(filename, 'w')
@@ -42,27 +63,27 @@ def write_settings(phenny, input, new_vars={}, new_steps={}):
         f.write('var|{0}|{1}\n'.format(key, val))
 
     for key, val in config_steps.items():
-        f.write('var|{0}|{1}\n'.format(key, val))
+        f.write('step|{0}|{1}\n'.format(key, val))
 
     f.close()
 
 
 def run_scripts(phenny, input):
     try:
-        file_command = "powershell %s\RunMe.ps1" % phenny.bot.tfs[input.nick]['path']
+        file_command = "C:\\Windows\\SysNative\\WindowsPowerShell\\v1.0\\powershell.exe %s\RunMe.ps1" % phenny.bot.tfs[input.nick]['path']
         subprocess.call(file_command)
-        phenny.reply("Done")
     except Exception, e:
         phenny.say(("dude, it totally failed: {0}").format(e))
 
 
-def tfs_local(phenny, input):
+def tfs_handler(phenny, input, new_vars, new_steps):
     if not phenny.bot.tfs[input.nick]['machine']:
         phenny.say(("Couldn't find your machine, {0}").format(input.nick))
     else:
-        phenny.say(("Building on {0}").format(phenny.bot.tfs[input.nick]['machine']))
-        write_settings(phenny, input)
+        phenny.say(("Starting on {0}").format(phenny.bot.tfs[input.nick]['machine']))
+        write_settings(phenny, input, new_vars, new_steps)
         run_scripts(phenny, input)
+        phenny.reply("Back to work, I'm done.")
 
 
 def tfs_publish(phenny, input):
@@ -76,21 +97,23 @@ def tfs_publish(phenny, input):
             path = server['path']
             break
 
-    if not environment:
-        phenny.say(("Couldn't find your machine, {0}").format(input.nick))
-    else:
-        phenny.say(("Publishing to {0}").format(environment))
-        write_settings(phenny, input, {"publishFolder": path, "buildConfig": environment}, {"Build": "true", "Publish": "true"})
-        run_scripts(phenny, input)
+    tfs_handler(phenny, input, {"publishFolder": path, "buildConfig": environment}, {"Build": "true", "Publish": "true"})
 
 
 def tfs_build(phenny, input):
-    phenny.say("coming soon")
+    branch = phenny.bot.tfs[input.nick]['branch']
+    tfs_handler(phenny, input, {"branch": branch, "workspaceName": branch}, {"CreateWorkspace": "true", "Build": "true", "GetLatest": "true", "MapIIS": "true"})
+
+
+def tfs_mapiis(phenny, input):
+    branch = phenny.bot.tfs[input.nick]['branch']
+    tfs_handler(phenny, input, {"branch": branch, "workspaceName": branch}, {"MapIIS": "true"})
 
 
 def tfs(phenny, input):
     """TFS Builder."""
     """build publish iis."""
+    print >> sys.stderr, "running tfs"
     parameters = input.group(2)
 
     if not hasattr(phenny.bot, 'tfs'):
@@ -128,34 +151,52 @@ def tfs(phenny, input):
     if command == "build":
         if not options:
             phenny.reply("Which branch?")
+            phenny.bot.tfs[input.nick]["question"] = "branch"
         else:
             tfs_build(phenny, input)
 
     elif command == "publish":
         if not options:
             phenny.reply("Which environment?")
+            phenny.bot.tfs[input.nick]["question"] = "environment"
         else:
+            phenny.bot.tfs[input.nick]['branch'] = options
             tfs_publish(phenny, input)
-
+    elif command == "mapiis":
+        if not options:
+            phenny.reply("Which branch?")
+            phenny.bot.tfs[input.nick]["question"] = "branch"
+        else:
+            phenny.bot.tfs[input.nick]['branch'] = options
+            tfs_mapiis(phenny, input)
     else:
-        tfs_local(phenny, input)
+        phenny.reply("I have no idea what you want me to do")
 
 tfs.commands = ['tfs']
-tfs.example = '.tfs build <branch> or .tfs (to run default script)'
+tfs.example = '.tfs build <branch> or .tfs publish <environment>'
 tfs.thread = True
 
 
-# def tfs_request(phenny, input):
-#     if not hasattr(phenny.bot, 'tfs'):
-#         return
+def tfs_request(phenny, input):
+    if not hasattr(phenny.bot, 'tfs'):
+        return
 
-#     if phenny.bot.tfs[input.sender] and phenny.bot.tfs[input.sender]['action']:
-#         phenny.bot.tfs[input.sender]['answer'] = input
-#         action = phenny.bot.tfs[input.sender]['action']
-#         tellee = input.nick
+    tellee = input.nick
 
-#         phenny.say(("I can {0} that for you, {1}").format(action, tellee))
+    if tellee in phenny.bot.tfs and 'question' in phenny.bot.tfs[tellee]:
+        question = phenny.bot.tfs[tellee]['question']
+        phenny.bot.tfs[tellee][question] = input
+        action = phenny.bot.tfs[tellee]['action']
 
-#         phenny.bot.tfs[input.sender] = None
+        del phenny.bot.tfs[tellee]['question']
 
-# tfs_request.rule = r'(.*)'
+        phenny.say(("I can {0} that for you, {1}").format(action, tellee))
+
+        if action == "build":
+            tfs_build(phenny, input)
+        elif action == "publish":
+            tfs_publish(phenny, input)
+        elif action == "mapiis":
+            tfs_mapiis(phenny, input)
+
+tfs_request.rule = r'(.*)'
